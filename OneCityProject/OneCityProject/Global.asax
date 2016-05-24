0971ddd8447a0cfd70e955f775e27788add862d6
mfcastro <mfcastro50@gmail.com>
2016-05-24 16:32:47 -0500

    First Commit

---
+++ OneCityProject/OneCityProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneCityProject.MvcApplication" Language="C#" %>
---
+++ OneCityProject/OneCityProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneCityProject.Startup))]
+namespace OneCityProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
